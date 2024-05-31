8f294d826444e13b10e3ad73bddec1d497f7abae
MayOwall <pixartive@gmail.com>
2024-05-31 16:00:02 +0900

    feat: pwa 적용

--- apps/myframe-web/vite.config.ts
+++ apps/myframe-web/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Myframe Prototype - Web",
+        short_name: "myframe-web",
+        description: "Myframe 프로토타입 - web 버전",
+        icons: [
+          {
+            src: "/icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
 });
